894aa67a16eb73b34f89b6f0d650001b0b80c021
Brett Marsh <brett.m.coding@gmail.com>
2018-10-10 02:32:45 -0400

    added background to bootscene. added load animation

--- js/BootScene.js
+++ js/BootScene.js
@@ -4,11 +4,78 @@ class BootScene extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image("3rdStrike", './assets/img/3rdStrike.jpg');
+    let progress = this.add.graphics();
+
+    this.load.on('progress', function (value) {
+
+      progress.clear();
+      progress.fillStyle(0xf8d838, 1);
+      progress.fillRect(0, 270, 800 * value, 60);
+
+    });
+
+    this.load.on('complete', function () {
+
+      progress.destroy();
+
+    });
+
+    for (let i = 1; i <= 8; i++) {
+      this.load.image("background" + i, "./assets/img/backgroundsprite/background" + i + ".png")
+      console.log("background" + i)
+    }
+    this.load.image('ground', './assets/img/platform.png');
+    this.load.image('skeleton', './assets/img/skeleton.png');
+    this.load.image('bloodchunk', './assets/img/particles/bloodchunk.png');
+    this.load.image('red', './assets/img/particles/red.png');
+    this.load.image('bone', './assets/img/particles/bone.png');
+    this.load.image('fire', './assets/img/particles/muzzleflash3.png');
+    this.load.image('flares', './assets/img/particles/sparkle1.png');
+    this.load.atlas('lazer', './assets/img/particles/lazer/lazer.png', './assets/img/particles/lazer/lazer.json');
+  
+    // Load Akuma sprites
+    this.load.multiatlas('akuma', './assets/spritesheets/akuma/akuma.json', './assets/spritesheets/akuma');
+  
+    // Load audio assets
+    this.load.audio('super', './assets/sounds/super.wav',);
+    this.load.audio('fiercekick', './assets/sounds/fiercekickA3.wav',);
+    this.load.audio('fiercepunch', './assets/sounds/fiercepunchA3.wav',);
+    this.load.audio('lightkick', './assets/sounds/lightkickA3.wav',);
+    this.load.audio('lightpunch', './assets/sounds/lightpunchA3.wav',);
+    this.load.audio('mediumpunch', './assets/sounds/mediumpunchA3.wav',);
+    this.load.audio('memescream', './assets/sounds/memescream.wav',);
+    
+    
+    // Load combo word
+    this.load.image('comboword', './assets/img/comboword.png');
+    
+    // this.alphabet is our array of A-Z strings that we use as keys to make new letter sprites
+    this.alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    // Load alphabet images (in a for loop to save space)
+    for (let i in this.alphabet) {
+      this.load.image(this.alphabet[i], "./assets/img/alphabet/"+this.alphabet[i]+".png");
+    }
   }
 
   create() {
-    this.add.image(400, 300, "3rdStrike");
+    this.anims.create({
+      key: "background",
+      frames: [
+        { key: "background1"},
+        { key: "background2"},
+        { key: "background3"},
+        { key: "background4"},
+        { key: "background5"},
+        { key: "background6"},
+        { key: "background7"},
+        { key: "background8"}
+      ],
+      frameRate: 15,
+      repeat: -1
+    });
+    this.background = this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, "background");
+
+    this.background.play("background");
 
     this.input.manager.enabled = true;
 
--- js/GameScene.js
+++ js/GameScene.js
@@ -6,51 +6,10 @@ class GameScene extends Phaser.Scene {
       super({ key: 'GameScene'});
   }
 
-preload(){
-  // Runs once and loads assets
-  // NOTE: "this" === Phaser.Scene
-
-  // Load image assets
-  // Loop for background frames
-  for (let i = 1; i <= 8; i++) {
-    this.load.image("background" + i, "./assets/img/backgroundsprite/background" + i + ".png")
-    console.log("background" + i)
-  }
-  this.load.image('ground', './assets/img/platform.png');
-  this.load.image('skeleton', './assets/img/skeleton.png');
-  this.load.image('bloodchunk', './assets/img/particles/bloodchunk.png');
-  this.load.image('red', './assets/img/particles/red.png');
-  this.load.image('bone', './assets/img/particles/bone.png');
-  this.load.image('fire', './assets/img/particles/muzzleflash3.png');
-  this.load.image('flares', './assets/img/particles/sparkle1.png');
-  this.load.atlas('lazer', './assets/img/particles/lazer/lazer.png', './assets/img/particles/lazer/lazer.json');
-
-  // Load Akuma sprites
-  this.load.multiatlas('akuma', './assets/spritesheets/akuma/akuma.json', './assets/spritesheets/akuma');
-
-  // Load audio assets
-  this.load.audio('super', './assets/sounds/super.wav',);
-  this.load.audio('fiercekick', './assets/sounds/fiercekickA3.wav',);
-  this.load.audio('fiercepunch', './assets/sounds/fiercepunchA3.wav',);
-  this.load.audio('lightkick', './assets/sounds/lightkickA3.wav',);
-  this.load.audio('lightpunch', './assets/sounds/lightpunchA3.wav',);
-  this.load.audio('mediumpunch', './assets/sounds/mediumpunchA3.wav',);
-  this.load.audio('memescream', './assets/sounds/memescream.wav',);
-  
-  
-  // Load combo word
-  this.load.image('comboword', './assets/img/comboword.png');
-  
-  // this.alphabet is our array of A-Z strings that we use as keys to make new letter sprites
-  this.alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  // Load alphabet images (in a for loop to save space)
-  for (let i in this.alphabet) {
-    this.load.image(this.alphabet[i], "./assets/img/alphabet/"+this.alphabet[i]+".png");
-  }
-  
-}
 create(){
-  
+  this.alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+
   // this.currentWord will be a random word from our Dictionary
   this.currentWord;
   
@@ -62,6 +21,7 @@ create(){
   
   // this.WORDS will be our imported dictionary of this.words in an array
   this.WORDS;
+
   // When this.timer hits 0 we will throw this to 1 to begin the end of round mechanics
   this.roundEndSwitch = 0;
   
@@ -80,21 +40,6 @@ create(){
   
   // Background
   // NOTE: We'll be using this.sys.game.config.width or height a lot to get the dimensions of our game
-  this.anims.create({
-    key: "background",
-    frames: [
-      { key: "background1"},
-      { key: "background2"},
-      { key: "background3"},
-      { key: "background4"},
-      { key: "background5"},
-      { key: "background6"},
-      { key: "background7"},
-      { key: "background8"}
-    ],
-    frameRate: 15,
-    repeat: -1
-  });
   this.background = this.add.sprite(this.sys.game.config.width / 2, this.sys.game.config.height / 2, "background");
   this.background.play("background");
 
